13a5c44c7c486d770ce1b8cddb1143f8b2bed127
Warren Postma <wpostma@ramsoft.com>
2016-07-05 16:59:25 -0400

    Bump assembly version info from alpha2 to alpha3 for Hl7.Fhir.DSTU2.0.90.5-alpha3

--- src/Hl7.Fhir.Core/Properties/AssemblyVersionInfo.cs
+++ src/Hl7.Fhir.Core/Properties/AssemblyVersionInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 // http://blogs.msdn.com/b/jjameson/archive/2009/04/03/best-practices-for-net-assembly-versioning.aspx
 [assembly: AssemblyVersion("0.90.5.0")]
 [assembly: AssemblyFileVersion("0.90.5.0")]
-[assembly: AssemblyInformationalVersion("Hl7.Fhir .Net Library 0.90.5-alpha2")]
+[assembly: AssemblyInformationalVersion("Hl7.Fhir .Net Library 0.90.5-alpha3")]
 
 #if !SIGNED
 [assembly: InternalsVisibleTo("Hl7.Fhir.Core.Tests")]
